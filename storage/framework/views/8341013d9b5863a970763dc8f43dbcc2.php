1a402dbaeda34f752eb421e7c7ea412d89792723
RicGonal <ricardo.gonzalezalv@temcilenio.mx>
2023-10-05 20:45:36 -0500

    homework 5

---
+++ storage/framework/views/8341013d9b5863a970763dc8f43dbcc2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+  <meta charset="UTF-8"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <title>View of Product</title> 
+</head> 
+<body> 
+  <h1>Product View</h1>
+  
+  <a href="/create"><button type="button">Add Product</button></a>
+</body> 
+</html> <?php /**PATH C:\Users\Tecmilenio\Documents\Template\mi-proyecto-laravel\resources\views/products/productView.blade.php ENDPATH**/ ?>
